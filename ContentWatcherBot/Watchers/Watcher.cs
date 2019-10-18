3afd4a5a772fc40d7e41371855d60fbf0528f41a
Corentin Crébier <bibo5088@gmail.com>
2019-10-18 19:10:14 +0200

    Initial commit, watcher

---
+++ ContentWatcherBot/Watchers/Watcher.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ContentWatcherBot.Watchers
+{
+    public abstract class Watcher
+    {
+        private static HttpClient _httpClient = new HttpClient();
+
+        public virtual string Name { get; }
+        public virtual string Description { get; }
+        protected virtual string UpdateMessage { get; }
+
+        private HashSet<string> _previousContentIds;
+
+        private Watcher()
+        {
+        }
+
+        public static async Task<T> Build<T>() where T : Watcher, new()
+        {
+            var watcher = new T();
+            var content = await watcher.FetchContent(_httpClient);
+
+            watcher._previousContentIds = content.Keys.ToHashSet();
+
+            return watcher;
+        }
+
+        protected abstract Task<IDictionary<string, string>> FetchContent(HttpClient client);
+
+        private async Task<IEnumerable<string>> NewContent()
+        {
+            var content = await FetchContent(_httpClient);
+
+            //Filter out previous content
+            var newContentKeysEnumerable = content.Keys.Except(_previousContentIds);
+            var newContentKeys = newContentKeysEnumerable as string[] ?? newContentKeysEnumerable.ToArray();
+            //Update _previousContent if needed
+            if (newContentKeys.Any())
+            {
+                _previousContentIds = newContentKeys.ToHashSet();
+            }
+
+            return newContentKeys.Select(key => content[key]);
+        }
+
+        public async Task<IEnumerable<string>> CheckAndGetMessages()
+        {
+            return (await NewContent()).Select(content => $"{UpdateMessage}\n${content}");
+        }
+    }
+}
