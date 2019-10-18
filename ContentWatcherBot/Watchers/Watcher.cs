8bd9c8948acd8804862b4d5ac7407b2ef0292f02
Corentin Crébier <bibo5088@gmail.com>
2019-10-18 20:02:01 +0200

    doc comments Watcher

--- ContentWatcherBot/Watchers/Watcher.cs
+++ ContentWatcherBot/Watchers/Watcher.cs
@@ -7,33 +7,65 @@ namespace ContentWatcherBot.Watchers
 {
     public abstract class Watcher
     {
-        private static readonly HttpClient _httpClient = new HttpClient();
+        private static readonly HttpClient HttpClient = new HttpClient();
 
+        /// <summary>
+        /// Name used to select this watcher, must be unique for every watcher, alpha num only
+        /// </summary>
+        /// <example>super_duper_webcomic</example>
         public abstract string Name { get; }
+
+        /// <summary>
+        /// Description for this watcher
+        /// </summary>
+        /// <example>Super Duper Webcomic</example>
         public abstract string Description { get; }
+
+        /// <summary>
+        /// Message that will be sent when new content shows up
+        /// </summary>
+        /// <example>New page of Super Duper Webcomic !</example>
         protected abstract string UpdateMessage { get; }
 
+        /// <summary>
+        /// Previous fetched IDs, used to detect new content
+        /// </summary>
         private HashSet<string> _previousContentIds;
 
         private Watcher()
         {
         }
 
+        /// <summary>
+        /// Build a new watcher and fill its _previousContentIds
+        /// </summary>
+        /// <typeparam name="T">A class derived from Watcher</typeparam>
+        /// <returns>Watcher of class T</returns>
+        /// <example><![CDATA[var superDuperWebcomicWatcher = Watcher.Build<SuperDuperWebcomicWatcher>()]]></example>
         public static async Task<T> Build<T>() where T : Watcher, new()
         {
             var watcher = new T();
-            var content = await watcher.FetchContent(_httpClient);
+            var content = await watcher.FetchContent(HttpClient);
 
             watcher._previousContentIds = content.Keys.ToHashSet();
 
             return watcher;
         }
 
+        /// <summary>
+        /// Fetch content from the source
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns>A dictionary, keys are ids and values are the content</returns>
         protected abstract Task<IDictionary<string, string>> FetchContent(HttpClient client);
 
+        /// <summary>
+        /// Fetch content from the source and filters out already known content, updates _previousContentIds if new content is found 
+        /// </summary>
+        /// <returns>A list of new content</returns>
         private async Task<IEnumerable<string>> NewContent()
         {
-            var content = await FetchContent(_httpClient);
+            var content = await FetchContent(HttpClient);
 
             //Filter out previous content
             var newContentKeysEnumerable = content.Keys.Except(_previousContentIds);
@@ -47,6 +79,10 @@ namespace ContentWatcherBot.Watchers
             return newContentKeys.Select(key => content[key]);
         }
 
+        /// <summary>
+        /// Fetch content from the source
+        /// </summary>
+        /// <returns>List of messages to send</returns>
         public async Task<IEnumerable<string>> CheckAndGetMessages()
         {
             return (await NewContent()).Select(content => $"{UpdateMessage}\n${content}");
