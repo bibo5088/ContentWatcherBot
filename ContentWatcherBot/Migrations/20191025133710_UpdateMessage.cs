42c051d16c4403dc69ba9f8b643064052b2dcd6d
Corentin Crébier <bibo5088@gmail.com>
2019-10-25 15:44:05 +0200

    UpdateMessage

---
+++ ContentWatcherBot/Migrations/20191025133710_UpdateMessage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContentWatcherBot.Migrations
+{
+    public partial class UpdateMessage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UpdateMessage",
+                table: "GuildWatchers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdateMessage",
+                table: "GuildWatchers");
+        }
+    }
+}
